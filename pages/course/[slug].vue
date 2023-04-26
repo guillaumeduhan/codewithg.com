35d69a1e1d537feaccc9f64f024664f457dbea7e
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-04-26 11:14:31 +0100

    commit

--- pages/course/[slug].vue
+++ pages/course/[slug].vue
@@ -68,9 +68,9 @@ useHead({
       </div>
     </header>
     <div class="flex items-center justify-center">
-      <button class="btn btn-primary">
+      <!-- <button class="btn btn-primary">
         Buy for ${{ getCourse.price }}
-      </button>
+      </button> -->
     </div>
   </div>
 </template>
