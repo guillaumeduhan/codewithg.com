da41f35dea2628b4728fdb555300557b45b69c19
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-04-30 18:21:41 +0100

    commit

--- components/Header.vue
+++ components/Header.vue
@@ -15,7 +15,10 @@ onMounted(async () => {
 
 <template>
   <div class="container grid grid-cols-2 py-6 Header">
-    <Logo />
+    <div class="flex items-center">
+      <Logo class="mr-2" />
+      <div class="uppercase label bg-primary-500">new</div>
+    </div>
     <div class="flex items-center justify-end">
       <div v-if="user" class="flex items-center justify-center">
         <button class="btn btn-info" @click="$router.push('/courses')">
