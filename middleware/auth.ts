55b45d79b92891649073e463da49beb7d3f589c4
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-04-30 11:57:10 +0100

    commit

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,16 +1,12 @@
 export default defineNuxtRouteMiddleware(async (to, _from) => {
   const supabase = useSupabaseAuthClient();
   const user = ref<any>(null);
-  try {
-    const response = await supabase.auth.getUser();
-    if (response) {
-      const { data } = response;
-      user.value = data.user;
-    } else {
-      navigateTo("/login");
-    }
-  } catch (error) {
-    console.error(error);
+  const response = await supabase.auth.getUser();
+  if (response) {
+    const { data } = response;
+    user.value = data.user;
+  }
+  if (!user.value) {
     navigateTo("/login");
   }
 });
