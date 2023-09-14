fdef4b553762dc3e604648ca14b19ed0ced7c771
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-09-14 15:52:47 +0100

    commit

--- components/Courses/List.vue
+++ components/Courses/List.vue
@@ -32,9 +32,9 @@ const getCourses = computed(() => {
   <div class="container CoursesList">
     <div v-if="!props.orders">
       <div class="flex gap-2 my-4">
-        <div @click="filter = item.code" v-for="(item, index) in langs" :key="index" :class="`text-xs font-medium cursor-pointer px-2 py-1 relative ${filter === item.code
-            ? 'bg-slate-800 text-white'
-            : 'bg-slate-100 text-gray-900'
+        <div @click="filter = item.code" v-for="(item, index) in langs" :key="index" :class="`text-base font-medium cursor-pointer px-2 py-1 relative ${filter === item.code
+          ? 'bg-slate-800 text-white'
+          : 'bg-slate-100 text-gray-900'
           }`">
           {{ item.title }}
           <div v-if="item.code === 'fr'" class="bg-primary-500 rounded-full h-2 w-2 absolute -top-1 -right-1"></div>
