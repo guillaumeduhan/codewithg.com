15d1876520bd5273bde7080da349e7a05af9b9e0
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-11-30 12:31:53 +0000

    commit

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
 definePageMeta({
   middleware: 'auth',
-  layout: 'school'
 })
+
+const { supabase } = useSupabase()
+const { getUser } = useStore()
 const router = useRouter()
-const supabase = useSupabaseAuthClient()
-const user = ref<any | null>(null)
-const response = await supabase.auth.getUser();
-if (response) {
-  const { data } = response;
-  user.value = data.user;
-}
 
 const logout = () => {
   supabase.auth.signOut()
@@ -20,20 +15,8 @@ const logout = () => {
 
 <template>
   <div class="container text-center Profile">
-    <header class="mb-8">
-      <h2 class="mb-4">My profile</h2>
-      <p>Find all informations concerning your account.</p>
-    </header>
-    <main v-if="user" class="mx-auto">
-      <div class="mb-12">
-        <Avatar :email="user.email" class="mb-4" />
-        <p class="mb-4 text-white">{{ user.email }}</p>
-      </div>
-      <div>
-        <button class="btn btn-primary" @click="logout">Logout</button>
-      </div>
-    </main>
+    <div>
+      <button class="btn btn-primary" @click="logout">Logout</button>
+    </div>
   </div>
 </template>
-
-<style lang='scss'></style>
