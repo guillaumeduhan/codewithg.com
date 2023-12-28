fa1ba8ff2eb8516d01074d4f1277b8254c1fdb21
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-12-28 22:06:19 +0100

    commit

--- components/Bento.vue
+++ components/Bento.vue
@@ -1,92 +1,38 @@
 <template>
-  <div class="bento grid gap-4">
-    <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-4">
+  <div class="bento grid gap-6">
+    <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-6">
       <div class="grid xl:col-span-1">
-        <div class="card card--youtube">
-          <h2 class="text-[40px]">Youtube</h2>
-          <p class="z-50">I created this YouTube channel in 2022 to share all my knowledge about web development. Since then, I've been posting 3 videos per week. Subscribe now!</p>
-          <div class="z-50">
-            <Button label="Subscribe now →"></Button>
-          </div>
-        </div>
+        <Card
+          title="Youtube"
+          description="I created this YouTube channel in 2022 to share all my knowledge about web development. Since then, I've been posting 3 videos per week. Subscribe now!"
+          label="Subscribe now →"
+        />
       </div>
-      <div class="grid xl:grid-cols-2 xl:col-span-2 gap-4">
-        <div>
-          <div class="card">
-            <h2>Coachings</h2>
+      <div class="grid lg:col-span-2 gap-6">
+        <div class="grid xl:grid-cols-2 xl:col-span-2 gap-6">
+          <Card
+            title="Freelancing"
+            description="Since 2015, I've held a place among the prestigious top 1% of freelancers on Malt, the leading freelance platform in Europe."
+            label="Contact →"
+          />
+          <Card
+            title="Coachings"
+            label="Book now →"
+          >
             <Coachings />
-            <div>
-              <Button label="Book now →"></Button>
-            </div>
-          </div>
+          </Card>
         </div>
-        <div>
-          <div class="card">
-            <h2>Coachings</h2>
-            <Coachings />
-            <div>
-              <Button label="Book now →"></Button>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div class="card">
-            <h2>Coachings</h2>
-            <Coachings />
-            <div>
-              <Button label="Book now →"></Button>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div class="card">
-            <h2>Coachings</h2>
-            <Coachings />
-            <div>
-              <Button label="Book now →"></Button>
-            </div>
-          </div>
-        </div>
-        <!-- <div>
-          <div class="card">
-            <h2>Courses</h2>
-          </div>
-        </div>
-        <div>
-          <div class="card">
-            <h2>Templates</h2>
-          </div>
-        </div>
-        <div>
-          <div class="card">
-            <h2>Snippets</h2>
-          </div>
-        </div> -->
-      </div>
-    </div>
-    <div class="grid xl:grid-cols-3 gap-4">
-      <div class="col-span-1">
-        <div class="card">
-          <h2>Hashnode</h2>
-          <div>
-            <Button label="Read →"></Button>
-          </div>
-        </div>
-      </div>
-      <div class="col-span-1">
-        <div class="card">
-          <h2>Dev.to</h2>
-          <div>
-            <Button label="Read →"></Button>
-          </div>
-        </div>
-      </div>
-      <div class="col-span-1">
-        <div class="card">
-          <h2>Medium</h2>
-          <div>
-            <Button label="Read →"></Button>
-          </div>
+        <div class="grid xl:grid-cols-2 xl:col-span-2 gap-6">
+          <Card
+            title="Newsletter"
+            description="Every Sunday, I send you a short email containing the 5 best links to articles, videos, or tweets about coding."
+            label="Subscribe →"
+          />
+          <Card
+            title="Blog on Hashnode"
+            description="In addition to videos, I write articles on Hashnode, tutorials, and guides to help people learn how to code."
+            label="Read →"
+          />
         </div>
       </div>
     </div>
---
+++ components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps(['title', 'description', 'label'])
+</script>
+
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <slot />
+    <div class="grid gap-6">
+      <p v-if="description">{{ description }}</p>
+      <div v-if="label">
+        <Button :label="label" />
+      </div>
+    </div>
+  </div>
+</template>
