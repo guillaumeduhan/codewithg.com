9b3d12f0a900044bc60603af52a526d50e9a9ca3
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-01-20 14:37:23 +0000

    commit

--- components/Bento.vue
+++ components/Bento.vue
@@ -1,3 +1,7 @@
+<script setup>
+const { openUrl } = useHelpers()
+</script>
+
 <template>
   <div class="bento grid gap-8">
     <Card class="lg:col-span-12" url="https://dub.sh/cvXSKdV">
@@ -12,36 +16,66 @@
       </Card>
     </div>
     <div class="lg:col-span-12 grid lg:grid-cols-12 gap-8">
-      <div class="supabase lg:col-span-12">
+      <div class="supabase lg:col-span-12" @click="openUrl('https://dub.sh/create-supabase')">
         <h3 class="text-3xl text-center">Supabase is the best Database as a Service since Firebase.</h3>
         <div class="flex items-center justify-center">
           <button class="mx-auto">Create an account →</button>
         </div>
       </div>
     </div>
+    <div class="lg:col-span-12 grid lg:grid-cols-12 gap-8">
+      <div class="lg:col-span-8 grid gap-8">
+        <Card url="https://dub.sh/codewithg">
+          <div class="rounded-xl overflow-hidden">
+            <img src="../assets/codewithg.png" />
+          </div>
+          <p>
+            <span class="text-white">Newsletter →</span>
+            Best coding news & tips.
+          </p>
+        </Card>
+      </div>
+      <div class="lg:col-span-4 grid gap-8">
+        <Card url="https://dub.sh/top-10-ctos">
+          <div class="rounded-xl overflow-hidden">
+            <img src="../assets/ibt.png" />
+          </div>
+          <p>
+            <span class="text-white">Top 10 CTos of 2024 →</span>
+            International Business Times.
+          </p>
+        </Card>
+        <Card url="https://dub.sh/6vbNPaH">
+          <p>
+            <span class="text-white">LinkedIn →</span> Follow me.
+          </p>
+        </Card>
+        <Card url="https://dub.sh/codewithg-blade">
+          <p>
+            <span class="text-white">Statistics →</span>
+            on Social Blade.
+          </p>
+        </Card>
+      </div>
+    </div>
     <!-- <div class="lg:col-span-12 grid lg:grid-cols-12 gap-8">
-      <div class="lg:col-span-4 grid gap-8">
-        <Card>
-          
-        </Card>
-        <Card>
-          
-        </Card>
+      <div class="malt lg:col-span-12" @click="openUrl('https://dub.sh/malt-codewithguillaume')">
+        <h3 class="text-3xl text-center">Malt, Europe's leading freelancing platform.</h3>
+        <div class="flex items-center justify-center">
+          <button class="mx-auto">Join now →</button>
+        </div>
       </div>
-      <div class="lg:col-span-4 grid gap-8">
-        <Card>
-          
-        </Card>
-        <Card>
-          
-        </Card>
-      </div>
-      <div class="lg:col-span-4 grid gap-8">
-        <Card>
-          
-        </Card>
-        <Card>
-          
+    </div>
+    <div class="lg:col-span-12 grid lg:grid-cols-12 gap-8">
+      <div class="lg:col-span-8 grid gap-8">
+        <Card url="https://dub.sh/codewithg-freelance">
+          <div class="rounded-xl overflow-hidden">
+            <img src="../assets/malt.png" />
+          </div>
+          <p>
+            <span class="text-white">Freelance →</span>
+            Malt, Europe's leading freelancing platform.
+          </p>
         </Card>
       </div>
     </div> -->
