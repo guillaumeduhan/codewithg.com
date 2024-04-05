b7e95838ef1826f0aed4e05bb4ffe6215b65629e
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-04-05 10:36:45 +0400

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,18 @@
+<script setup>
+const { openUrl } = useHelpers();
+
+</script>
+
 <template>
-  <div class="relative py-4 lg:py-12 overflow-hidden">
-    <div class="px-4 lg:px-12 max-w-[1400px] mx-auto">
+  <div class="relative overflow-hidden">
+    <div @click="openUrl('https://dub.sh/thedevelopercourse')"
+      class="bg-green-500 w-full px-2 py-2 text-center text-black text-sm font-[600] cursor-pointer hover:bg-green-500">
+      <span>-50% on thedeveloper.com — private lessons with exclusive access, released next July.</span>
+    </div>
+    <div class="px-4 lg:px-12 lg:py-12 max-w-[1400px] mx-auto">
       <Header />
       <slot />
       <Footer />
-      <Offer />
     </div>
   </div>
 </template>
