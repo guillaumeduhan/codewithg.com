f299399856d722654dadc0f9ecdf0b3a3cd7e91d
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-06-09 23:48:24 +0200

    commit

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,24 @@
 <template>
-  <header class="grid gap-4 container mx-auto">
+  <header class="container grid gap-4 mx-auto">
     <h1><span class="font-bold">Codewithg.com</span>. CTO of Allocations.com, previously Google, freelance for 15 years. Running a YouTube channel: @codewithguillaume. Currently living in Dubai.</h1>
     <div class="flex gap-2">
-      <UiButton>Masterclass — $12</UiButton>
-      <UiButton class="flex gap-1 bg-red-600 hover:bg-red-500 dark:bg-red-600 dark:text-white">
+      <button class="primary">Masterclass — $12</button>
+      <button class="youtube">
         <IconsYoutube />
         <span>178k</span>
-      </UiButton>
+      </button>
     </div>
   </header>
 </template>
+
+<style scoped>
+button {
+  @apply px-4 py-2 rounded text-sm text-white;
+  &.primary {
+    @apply bg-neutral-900 dark:bg-neutral-800 text-white hover:bg-neutral-800;
+  }
+  &.youtube {
+    @apply flex gap-1 bg-red-600 hover:bg-red-500 dark:bg-red-600 dark:text-white;
+  }
+}
+</style>
