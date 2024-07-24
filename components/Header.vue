6e2dcef4768877e863fff7a17bf0be9659be9e68
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-07-24 11:55:16 +0200

    commit

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <script setup>
-const { openUrl } = useHelpers();
+const { openUrl } = useHelpers();
 </script>
 
 <template>
@@ -15,11 +15,25 @@ const { openUrl } = useHelpers();
       </div>
     </div>
     <div class="flex gap-2">
-      <button class="primary" @click="openUrl('https://dub.sh/thedevelopercourse')">Masterclass — $12</button>
       <button class="youtube">
         <IconsYoutube />
-        <span>178k</span>
+        <span>Youtube channel</span>
       </button>
+    </div>
+    <div class="grid items-center gap-4 px-4 py-3 border rounded cursor-pointer dark:border-slate-800"
+      @click="openUrl('https://dub.link/devcourse')">
+      <div class="grid gap-4 lg:grid-cols-3">
+        <img class="rounded shadow" src="/next.jpg" alt="next" />
+        <img class="rounded shadow" src="/supabase.jpg" alt="next" />
+        <img class="rounded shadow" src="/nest.jpg" alt="next" />
+      </div>
+      <div class="grid gap-1 text-center">
+        <h2 class="font-[600] text-xl">thedevelopercourse.com</h2>
+        <p>A Masterclass for Full-Stack Devs I wish I had ten years ago.</p>
+        <div>
+          <button class="font-[600] bg-emerald-500">Get access for $12</button>
+        </div>
+      </div>
     </div>
   </header>
 </template>
@@ -27,9 +41,11 @@ const { openUrl } = useHelpers();
 <style scoped>
 button {
   @apply px-4 py-2 rounded text-sm text-white;
+
   &.primary {
     @apply bg-neutral-900 dark:bg-neutral-800 text-white hover:bg-neutral-800;
   }
+
   &.youtube {
     @apply flex gap-1 bg-red-600 hover:bg-red-500 dark:bg-red-600 dark:text-white;
   }
