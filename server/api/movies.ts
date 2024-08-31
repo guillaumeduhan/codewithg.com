0864744e17ba3efee8bc87f155c168c9b72c663d
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2024-08-31 17:43:46 +0200

    commit

--- server/api/movies.ts
+++ server/api/movies.ts
@@ -1,40 +1,39 @@
+let movies = [
+  {
+    title: "Inception",
+    director: "Christopher Nolan",
+    year: 2010
+  },
+  {
+    title: "The Matrix",
+    director: "Lana Wachowski, Lilly Wachowski",
+    year: 1999
+  },
+  {
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    year: 2014
+  },
+  {
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: 1972
+  },
+  {
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: 1994
+  }
+];
 export default defineEventHandler(async (event) => {
-  // Shared movies array
-  let movies = [
-    {
-      title: "Inception",
-      director: "Christopher Nolan",
-      year: 2010
-    },
-    {
-      title: "The Matrix",
-      director: "Lana Wachowski, Lilly Wachowski",
-      year: 1999
-    },
-    {
-      title: "Interstellar",
-      director: "Christopher Nolan",
-      year: 2014
-    },
-    {
-      title: "The Godfather",
-      director: "Francis Ford Coppola",
-      year: 1972
-    },
-    {
-      title: "Pulp Fiction",
-      director: "Quentin Tarantino",
-      year: 1994
-    }
-  ];
 
   // Determine the HTTP method
-  if (event.req.method === 'GET') {
+  if (event.method === 'GET') {
     // Handle GET request
     return {
       movies
     };
-  } else if (event.req.method === 'POST') {
+  } else if (event.method === 'POST') {
     // Handle POST request
     const body = await readBody(event);
     movies = [...movies, body]; // Add the new movie to the array
