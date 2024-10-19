c11d6cdce9d3b0ef984429bd6fc4717ac68a4834
guillaumeduhan <myhappyagency@gmail.com>
2024-10-19 12:34:18 +0200

    commit

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -80,9 +80,9 @@ const TodoList: React.FC = () => {
   }
 
   return (
-    <div className="max-w-[800px] grid gap-4">
-      <header className="border border-neutral-200 dark:border-slate-800 rounded-xl overflow-hidden">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6 bg-neutral-50 dark:bg-transparent px-3 py-2">
+    <div className="max-w-[600px] grid gap-4">
+      <header>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6 px-3 py-2">
           <div>
             <p className="text-2xl">{totalTasks}</p>
             <h2>Tasks</h2>
@@ -96,27 +96,37 @@ const TodoList: React.FC = () => {
             <h2>Average time</h2>
           </div>
         </div>
-        <div className="flex gap-1 px-3 pb-2">
-          <Input
-            type="text"
-            value={newTask}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTask(e.target.value)}
-            placeholder="Add a new task"
-          />
-          <div>
-            <Button onClick={addTask}>Save new task</Button>
+        <div className="grid gap-4 bg-neutral-50 dark:bg-transparent rounded-xl border dark:border-slate-800 px-4 py-6">
+          <div className="shadow rounded border mx-auto p-1 shadow -rotate-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="2"><rect width="4" height="4" x="16" y="9" fill="currentColor" rx="2" transform="rotate(90 16 9)" /><rect width="4" height="4" x="20" y="17" fill="currentColor" rx="2" transform="rotate(90 20 17)" /><path d="M5 4v11c0 1.886 0 2.828.586 3.414S7.114 19 9 19h7" /><path d="M5 7c0 1.886 0 2.828.586 3.414S7.114 11 9 11h3" /></g></svg>
+          </div>
+          <div className="text-center">
+            <p className="font-[600]">Save a new task</p>
+            <p className="text-neutral-400 mb-4">All tasks here are going to be saved locally.</p>
+            <div className="grid gap-2">
+              <Input
+                className="bg-white text-center"
+                type="text"
+                value={newTask}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTask(e.target.value)}
+                placeholder="Add a new task"
+              />
+              <div>
+                <Button onClick={addTask} className="mx-auto">Save new task</Button>
+              </div>
+            </div>
           </div>
         </div>
       </header>
 
-      <div className="grid gap-1 bg-neutral-50 dark:bg-transparent border border-neutral-200 dark:border-slate-800 rounded-xl overflow-hidden">
+      <div className="grid gap-1">
         {Object.entries(groupedTasks).map(([date, dateTasks]) => (
           <div key={date} className="grid gap-1">
             <div className="px-3 py-2 dark:bg-slate-800">{date} ({dateTasks.length})</div>
             <div className="grid gap-2 bg-white dark:bg-transparent py-2">
-              {dateTasks.map((task: Task) => (
-                <div key={task.id} className="flex items-center cursor-pointer transition group px-3 py-1 dark:hover:bg-slate-800">
-                  <div onClick={() => toggleTask(task.id)} className="w-full flex gap-2">
+              {dateTasks.map((task: Task, i: number) => (
+                <div key={task.id} className={`flex items-center gap-2 cursor-pointer bg-white dark:bg-transparent transition group px-4 py-3 dark:hover:bg-slate-800 border dark:border-slate-800 rounded-lg shadow z-${i + 1}`}>
+                  <div onClick={() => toggleTask(task.id)} className="w-full grid gap-2">
                     {!task.done && <Circle className="text-neutral-300" />}
                     {task.done && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m10.6 13.8l-2.15-2.15q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7L9.9 15.9q.3.3.7.3t.7-.3l5.65-5.65q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" /></svg>}
                     <div>{task.title}</div>
