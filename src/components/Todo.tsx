471d5ec110f8798f6d341c5d27d57c4ab0baee56
guillaumeduhan <myhappyagency@gmail.com>
2024-10-23 11:26:35 +0200

    commit

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -143,8 +143,8 @@ const TodoList: React.FC = () => {
           })
           .map((task: Task, i: number) => (
             <div key={task.id} className={`flex items-center gap-2 cursor-pointer bg-white dark:bg-transparent transition group px-4 py-3 dark:hover:bg-slate-800 border dark:border-slate-800 rounded-lg shadow z-${i + 1}`}>
-              <div onClick={() => toggleTask(task.id)} className="w-full grid gap-2">
-                <div className="flex items-start justify-between">
+              <div className="w-full grid gap-2">
+                <div className="flex items-start justify-between" onClick={() => toggleTask(task.id)}>
                   {!task.done && <Circle className="text-neutral-300" />}
                   {task.done && <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m10.6 13.8l-2.15-2.15q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7L9.9 15.9q.3.3.7.3t.7-.3l5.65-5.65q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" /></svg>}
                   <span className="text-neutral-500">{dayjs(task.created_at).format('dddd DD MMM')}</span>
